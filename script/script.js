91505b27dd67791c44c4eee80b20923f3a4d0a3d
burnedsap <4111699+burnedsap@users.noreply.github.com>
2023-04-12 18:34:35 -0400

    Adding updated content

---
+++ script/script.js
@@ -0,0 +1,10 @@
+// Salil Parekh is a design researcher and creative technologist. He is currently working on creating <a href="https://theplanetarybureau.com">The Planetary Bureau</a> at The New School, New York.
+
+let intro = [
+  "He is currently working on creating Scuffed Embroidery",
+  "He is currently working at EY wavespace.",
+  "He is currently working on Haboob",
+  "He is currently hacking electronic devices.",
+  "He is currently working on this website.",
+  'He is currently working on creating <a href="https://theplanetarybureau.com">The Planetary Bureau</a>',
+];
